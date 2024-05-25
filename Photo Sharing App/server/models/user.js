bf49aaa6dbd6afc3f168c3d37d8ef17529eb046d
Atharv Dange <atharvdange618@gmail.com>
2024-05-25 13:16:04 +0530

    fix: changed db from local to atlas

--- Photo Sharing App/server/models/user.js
+++ Photo Sharing App/server/models/user.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-mongoose.connect("mongodb://localhost:27017/reactpractice");
+mongoose.connect("mongodb+srv://atharvdangedev:<password>@cluster0.bxjpvk9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    dbName: "RESTAURANT_APPLICATION"
+}).then(() => {
+    console.log("Connected to the database")
+}).catch((err) => {
+    console.error(err.message)
+});
+
 
 //model for storing users
 const userSchema = new mongoose.Schema({
