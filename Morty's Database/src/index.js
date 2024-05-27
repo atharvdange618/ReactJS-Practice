9847d09817ba1449d24140408d8990e88ed0d1d2
Atharv Dange <atharvdange618@gmail.com>
2024-05-28 00:47:03 +0530

    Project Completed

--- Morty's Database/src/index.js
+++ Morty's Database/src/index.js
@@ -1,15 +1,20 @@
 import ReactDOM from 'react-dom/client';
-import React from 'react'; // Ensure React is imported
+import List from './components/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    return <h1>Rick and Morty</h1>;
+    return (
+        <div>
+            <nav className='navbar sticky-top navbar-dark bg-dark'>
+                <h1 className='navbar-brand text-light mx-2 px-2'>Rick and Morty</h1>
+            </nav>
+            <List />
+        </div>
+    );
 }
 
 const container = document.getElementById('root');
 
-if (container) {
-    const root = ReactDOM.createRoot(container);
-    root.render(<App />);
-} else {
-    console.error('Failed to find the root element.');
-}
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);   
