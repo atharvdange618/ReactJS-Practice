8fb6a159d7e33078f31aebc91f720b82427ae1d9
Atharv Dange <atharvdange618@gmail.com>
2024-06-13 17:46:00 +0530

    test for deployment on vercel

--- SmartHierarchy/Frontend/src/Components/Home/Home.jsx
+++ SmartHierarchy/Frontend/src/Components/Home/Home.jsx
@@ -2,12 +2,6 @@ import React, { useEffect } from 'react';
 
 const Home = () => {
 
-    useEffect(() => {
-        const feat = fetch("/api")
-        console.log("fetch request sent")
-    }, [])
-
-
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold">Home</h1>
